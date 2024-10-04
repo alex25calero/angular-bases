a8135791bab03e616ba860f60fc70450ecea83f7
Alejandro <alex25calero@gmail.com>
2024-10-04 13:16:58 +0200

    Fin de la seccion 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { CounterModule } from './counter/components/counter.module';
+
 import { AppComponent } from './app.component';
+import { HeroModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    CounterModule,
+    HeroModule,
+    DbzModule
+
+
+],
   providers: [],
   bootstrap: [AppComponent]
 })
